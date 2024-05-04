f107b0a537b8eead684c09b5435b780f4f75bec3
W Cardoso <cardosu7@gmail.com>
2024-05-04 18:47:43 -0300

    exercicio conjunto de convidados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1236);
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1237);
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1238);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1238);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de convidados");
+
     }
 }
